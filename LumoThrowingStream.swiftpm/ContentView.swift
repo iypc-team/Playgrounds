0027b7cf7451906b981a7c3d27f6dc33558f7169
iypc-team <50553466+iypc-team@users.noreply.github.com>
2025-10-05 11:07:08 -0500

    LumoThrowingStream  10/05/2025-1

---
+++ LumoThrowingStream.swiftpm/ContentView.swift
@@ -0,0 +1,16 @@
+// LumoThrowingStream  10/05/2025-1
+// CoreMotion attitude quaternion AsyncThrowingStream
+// 
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, nucklehead!")
+        }
+        .font(.title)
+    }
+}
