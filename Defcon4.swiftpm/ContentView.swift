0574096018ead2cbde9fb0610c74a0269ce0df6f
iypc-team <50553466+iypc-team@users.noreply.github.com>
2025-10-25 15:22:59 -0500

    Defcon4  10/25/2025-1

--- Defcon4.swiftpm/ContentView.swift
+++ Defcon4.swiftpm/ContentView.swift
@@ -1,17 +1,14 @@
-//  Defcon4  10/18/2025-2
+//  Defcon4  10/25/2025-1
 //  green
 
 import SwiftUI
 import UIKit
-import Dispatch
 import SceneKit
-import GameKit
 import Foundation
-//import RealityKit
 
 //struct Universe: UIViewRepresentable {
 //        typealias UV = Universe
-//        var universeScene: SCNScene? = SCNScene(named: "bullshit")
+//        var universeScene: SCNScene? = SCNScene(named: "universe")
 //        var universe: SCNSphere? = SCNNode(geometry: .sphere)
 //        var universeNode: SCNNode? = SCNNode.init(geometry: universe)
 //        
@@ -40,7 +37,7 @@ struct SceneKitView : UIViewRepresentable {
         
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
-        lightNode.light!.color = UIColor.darkGray
+        lightNode.light!.color = UIColor.clear
         lightNode.light!.type = .ambient
         lightNode.position = SCNVector3(x: 0, y: 10, z: 100)
         lightNode.light!.intensity = 500
@@ -50,14 +47,14 @@ struct SceneKitView : UIViewRepresentable {
         lightNode_2.light = SCNLight()
         lightNode_2.light!.type = .ambient
         lightNode_2.position = SCNVector3(x: 0, y: 0, z: -100)
-        lightNode_2.light!.color = UIColor.darkGray
+        lightNode_2.light!.color = UIColor.clear
         lightNode_2.light!.intensity = 100
         scene.rootNode.addChildNode(lightNode_2)
         
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
+        ambientLightNode.light!.color = UIColor.clear
         ambientLightNode.light!.intensity = 10
         scene.rootNode.addChildNode(ambientLightNode)
         
@@ -65,7 +62,7 @@ struct SceneKitView : UIViewRepresentable {
         shipLightNode.position = SCNVector3(x: 0, y: 0, z: 0)
         shipLightNode.light = SCNLight()
         shipLightNode.light!.type = .omni
-        shipLightNode.light!.intensity = 1000
+        shipLightNode.light!.intensity = 500
         shipLightNode.light!.color =  UIColor.green
         scene.rootNode.addChildNode(shipLightNode)
         
@@ -73,7 +70,7 @@ struct SceneKitView : UIViewRepresentable {
         shipLightNode2.position = SCNVector3(x: 0, y: 0, z: 0)
         shipLightNode2.light = SCNLight()
         shipLightNode2.light!.type = .omni
-        shipLightNode2.light!.intensity = 1000
+        shipLightNode2.light!.intensity = 500
         shipLightNode2.light!.color =  UIColor.green
         scene.rootNode.addChildNode(shipLightNode2)
         
@@ -113,7 +110,7 @@ struct SceneKitView : UIViewRepresentable {
         tube.materials = [material]
         
         // Wrap the geometry in a node
-        var xAxis = SCNNode(geometry: tube)
+        let xAxis = SCNNode(geometry: tube)
         
         // Position the node in the scene
         xAxis.position = SCNVector3(x: 0, y: 0, z: 0)
@@ -137,7 +134,7 @@ struct SceneKitView : UIViewRepresentable {
         tube.materials = [material]
         
         // Wrap the geometry in a node
-        var zAxis = SCNNode(geometry: tube)
+        let zAxis = SCNNode(geometry: tube)
         
         // Position the node in the scene
         zAxis.position = SCNVector3(x: 0, y: 0, z: 0)
@@ -160,7 +157,7 @@ struct SceneKitView : UIViewRepresentable {
         scnView.autoenablesDefaultLighting = true
         scnView.allowsCameraControl = true
         scnView.showsStatistics = true
-        scnView.backgroundColor = UIColor.darkGray
+        scnView.backgroundColor = UIColor.clear
     }
 }
 
