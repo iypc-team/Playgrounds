5abca823d1b677053c70f15568b6b658019c2a79
iypc-team <50553466+iypc-team@users.noreply.github.com>
2025-11-05 16:18:55 -0600

    Framework Helper  11/05/2026- initial commit

---
+++ Framework Helper.swiftpm/ContentView.swift
@@ -0,0 +1,15 @@
+// Framework Helper  11/05/2026- initial commit
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
