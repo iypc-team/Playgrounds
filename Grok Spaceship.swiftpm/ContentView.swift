2d27485cf12ef18ae32f02e03136d64f6d12ef6f
iypc-team <50553466+iypc-team@users.noreply.github.com>
2025-11-28 13:01:06 -0600

    Grok Spaceship  11/28/2025-2

--- Grok Spaceship.swiftpm/ContentView.swift
+++ Grok Spaceship.swiftpm/ContentView.swift
@@ -1,6 +1,5 @@
 // Grok Spaceship  11/28/2025-1
-// SwiftUI + RealityKit, MVVM, iOS 16, loadModel(Airplane.usdz), lighting, gesture, recognizers, 
-// Main actor-isolated 'transform' can nor be mutated from non-isolated context
+// 
 
 import SwiftUI
 
