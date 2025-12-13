7c2322f9c308ded9b3f1e82f249048ad32465f87
iypc-team <50553466+iypc-team@users.noreply.github.com>
2025-12-13 10:48:52 -0600

    Defcon4 MVVM  12/13/2025-1

---
+++ Defcon4 MVVM.swiftpm/SceneModel.swift
@@ -0,0 +1,46 @@
+// 
+// 
+
+import SceneKit
+import UIKit
+
+class SceneModel {
+    // 3D Scene
+    var scene: SCNScene?
+    var cameraNode: SCNNode?
+    var lightNodes: [SCNNode] = []
+    
+    init(sceneName: String, cameraPosition: SCNVector3) {
+        // Load the scene
+        self.scene = SCNScene(named: sceneName)
+        
+        // Configure the camera node
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.camera?.automaticallyAdjustsZRange = true
+        cameraNode.position = cameraPosition
+        self.cameraNode = cameraNode
+        
+        // Add the camera node to the scene
+        self.scene?.rootNode.addChildNode(cameraNode)
+        
+        // Configure lights
+        configureDefaultLights()
+    }
+    
+    private func configureDefaultLights() {
+        // Primary light source
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .ambient
+        lightNode.light!.color = UIColor.white
+        lightNode.light!.intensity = 1000
+        lightNodes.append(lightNode)
+        
+        lightNode.position = SCNVector3(x: 0, y: 100, z: 100)
+        scene?.rootNode.addChildNode(lightNode)
+        
+        // Additional lights can be added here
+    }
+}
+
