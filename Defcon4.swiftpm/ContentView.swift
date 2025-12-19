3a1009a890178e37d4b514132bd577515e686be9
Ralph Krueger <ralph.krueger@icloud.com>
2025-12-19 15:26:53 -0600

    Defcon4 12/19/2025-4

--- Defcon4.swiftpm/ContentView.swift
+++ Defcon4.swiftpm/ContentView.swift
@@ -1,8 +1,8 @@
-//  Defcon4 12/19/2025-3
+//  Defcon4 12/19/2025-4
 /*
  https://github.com/iypc-team/Playgrounds/tree/main/Defcon4.swiftpm
  */
-// 
+// 2.0
 
 import SwiftUI
 
@@ -29,7 +29,12 @@ struct SceneKitView: UIViewRepresentable {
         scnView.scene?.background.contents = UIColor.lightGray
         scnView.allowsCameraControl = true  
         scnView.autoenablesDefaultLighting = true
-        scnView.antialiasingMode = .multisampling2X
+        scnView.antialiasingMode = .multisampling4X
+        
+        // Add this to scale the fighter node
+        if let fighterNode = scene.rootNode.childNode(withName: "fighter", recursively: true) {
+            fighterNode.scale = SCNVector3(x: 0.5, y: 0.5, z: 0.5)  // Adjust scale as needed
+        }
         
         return scnView
     }
