83b947ea89091776d347fdfbd05bd7b6d65ebaa6
Ralph Krueger <ralph.krueger@icloud.com>
2026-01-10 12:53:52 -0600

    Framework Helper  01/10/2026-2

--- Framework Helper.swiftpm/ContentView.swift
+++ Framework Helper.swiftpm/ContentView.swift
@@ -1,22 +1,53 @@
-// Framework Helper  01/10/2026-1
+// Framework Helper  01/10/2026-2
 // 
 // https://github.com/iypc-team/Playgrounds/tree/main/Framework%20Helper.swiftpm
 // 
 
 import SwiftUI
 
+//
+// 
+
+import SwiftUI
+
 struct ContentView: View {
     @StateObject private var viewModel = FrameworksViewModel()
+    @State private var searchText = ""
+    
+    var filteredFrameworks: [Framework] {
+        if searchText.isEmpty {
+            return viewModel.frameworks
+        } else {
+            return viewModel.frameworks.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
     
     var body: some View {
         NavigationView {
-            List(viewModel.frameworks) { framework in
+            List(filteredFrameworks) { framework in
                 Text(framework.name)
             }
             .navigationTitle("Frameworks")
+            .searchable(text: $searchText, prompt: "Search frameworks")
             .onAppear {
                 viewModel.fetchFrameworks()
             }
         }
     }
 }
+
+//struct ContentView: View {
+//    @StateObject private var viewModel = FrameworksViewModel()
+//    
+//    var body: some View {
+//        NavigationView {
+//            List(viewModel.frameworks) { framework in
+//                Text(framework.name)
+//            }
+//            .navigationTitle("Frameworks")
+//            .onAppear {
+//                viewModel.fetchFrameworks()
+//            }
+//        }
+//    }
+//}
--- Framework Helper.swiftpm/FrameworksListView.swift
+++ Framework Helper.swiftpm/FrameworksListView.swift
@@ -3,19 +3,68 @@
 
 import Foundation
 
+//
+// 
+
+import Foundation
+
 class FrameworksViewModel: ObservableObject {
     @Published var frameworks: [Framework] = []
     
+    // Complete list of known Apple frameworks for iOS development
+    private let knownFrameworks = [
+        "SwiftUI",
+        "UIKit",
+        "Foundation",
+        "CoreData",
+        "Combine",
+        "ARKit",
+        "QuartzCore", // Core Animation
+        "CoreGraphics", // Core Graphics (Quartz)
+        "CoreLocation",
+        "MapKit",
+        "AVFoundation",
+        "SpriteKit",
+        "SceneKit",
+        "Metal",
+        "CoreML",
+        "Vision",
+        "HealthKit",
+        "WatchKit",
+        "CloudKit",
+        "StoreKit",
+        "PassKit",
+        "PushKit",
+        "CallKit",
+        "EventKit",
+        "HomeKit",
+        "UserNotifications",
+        "Contacts",
+        "MetricKit",
+        "CreateML",
+        "RealityKit",
+        "NetworkExtension"
+    ]
+    
     func fetchFrameworks() {
-        // Fetching all frameworks
-        let bundles = Bundle.allFrameworks
-        self.frameworks = bundles.compactMap { bundle in
-            // Extracting the framework name
-            guard let name = bundle.bundlePath.components(separatedBy: "/").last else {
-                return nil
-            }
-            return Framework(name: name)
-        }
+        // Use the known list instead of loaded frameworks
+        self.frameworks = knownFrameworks.map { Framework(name: $0) }
     }
 }
+
+//class FrameworksViewModel: ObservableObject {
+//    @Published var frameworks: [Framework] = []
+//    
+//    func fetchFrameworks() {
+//        // Fetching all frameworks
+//        let bundles = Bundle.allFrameworks
+//        self.frameworks = bundles.compactMap { bundle in
+//            // Extracting the framework name
+//            guard let name = bundle.bundlePath.components(separatedBy: "/").last else {
+//                return nil
+//            }
+//            return Framework(name: name)
+//        }
+//    }
+//}
 
