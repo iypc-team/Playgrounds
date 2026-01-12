c3203c6bf762d11648c14148acf159ae7e62d7c3
Ralph Krueger <ralph.krueger@icloud.com>
2026-01-11 18:03:56 -0600

    Framework Helper  01/11/2026-9

--- Framework Helper.swiftpm/ContentView.swift
+++ Framework Helper.swiftpm/ContentView.swift
@@ -1,7 +1,12 @@
-// Framework Helper  01/11/2026-8
+// Framework Helper  01/11/2026-9
 // 
 //  https://github.com/iypc-team/Playgrounds/tree/main/Framework%20Helper.swiftpm
-//  
+/*  
+A NavigationLink is presenting a value of type “Framework” but there is no matching navigationDestination declaration visible from the location of the link. The link cannot be activated.
+ 
+ Note: Links search for destinations in any surrounding NavigationStack, then within the same column of a NavigationSplitView.
+ */
+
 
 import SwiftUI
 
@@ -17,6 +22,9 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Libraries")
+        }
+        .navigationDestination(for: Framework.self) { framework in 
+            Text("Details for \(framework.name)")
         }
     }
 }
