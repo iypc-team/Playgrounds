ab1d21f20e7e87edc48309d56324703c55553958
Ralph Krueger <ralph.krueger@icloud.com>
2026-01-16 15:09:06 -0600

    Icosahedron  01/16/2026-1

--- Icosahedron.swiftpm/ContentView.swift
+++ Icosahedron.swiftpm/ContentView.swift
@@ -1,7 +1,12 @@
+//  Icosahedron  01/16/2026-1
+//  https://github.com/iypc-team/Playgrounds/tree/main/Icosahedron.swiftpm
+// 
+
 import SwiftUI
 
 struct ContentView: View {
     let gvc = GameViewController()
+    
     var body: some View {
         VStack {
             Text("gvc: \(gvc)")
@@ -9,12 +14,12 @@ struct ContentView: View {
             Text("children.count: \(gvc.classForCoder )")
             Text("shouldAutorotate: \(gvc.shouldAutorotate)")
             Text("prefersStatusBarHidden: \(gvc.prefersStatusBarHidden)")
-//            Text("gvc: \(gvc)")
-//            Text("gvc: \(gvc)")
-//            Text("gvc: \(gvc)")
+            //            Text("gvc: \(gvc)")
+            //            Text("gvc: \(gvc)")
+            //            Text("gvc: \(gvc)")
         }
         .multilineTextAlignment(.leading)
-        .font(.system(size: 18, weight: .bold, design: .default))
-        .padding(2)
+        .font(.system(size: 18, weight: .regular, design: .default))
+        .padding(10)
     }
 }
