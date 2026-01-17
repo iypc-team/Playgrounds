0b7f673ab5409d8033c97138565618c4480d278b
Ralph Krueger <ralph.krueger@icloud.com>
2026-01-17 09:11:42 -0600

    Icosahedron  01/17/2026-1

--- Icosahedron.swiftpm/ContentView.swift
+++ Icosahedron.swiftpm/ContentView.swift
@@ -1,4 +1,4 @@
-//  Icosahedron  01/16/2026-4
+//  Icosahedron  01/17/2026-1
 //  Copyright © 2018 IYPC Software. All rights reserved.
 //  https://github.com/iypc-team/Playgrounds/tree/main/Icosahedron.swiftpm
 // 
@@ -8,16 +8,31 @@ import UIKit
 
 struct ContentView: View {
     var body: some View {
-        GameViewControllerRepresentable()
+        ZStack {
+            Color.black // Set the background color to black
+                .edgesIgnoringSafeArea(.all) // Ensure it covers the entire screen
+            
+            GameViewControllerRepresentable()
+                .onAppear {
+                    print("GameViewControllerRepresentable loaded.")
+                }
+                .onDisappear {
+                    print("GameViewControllerRepresentable unloaded.")
+                }
+        }
+        .overlay(Text("Loading...") // Add a fallback text in case it takes time to load
+            .foregroundColor(.white))
     }
 }
 
 struct GameViewControllerRepresentable: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> GameViewController {
+        // Ensure GameViewController is loading correctly; fallback debug
         return GameViewController()
     }
     
     func updateUIViewController(_ uiViewController: GameViewController, context: Context) {
+        print("GameViewController updated.")
         // No updates needed for this static view
     }
 }
