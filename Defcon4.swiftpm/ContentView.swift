038260f438ffce6baecc810307a932fcefa1e331
Ralph Krueger <ralph.krueger@icloud.com>
2026-02-02 18:59:20 -0600

    Defcon4 02/02/2026-3

--- Defcon4.swiftpm/ContentView.swift
+++ Defcon4.swiftpm/ContentView.swift
@@ -1,26 +1,23 @@
-//  Defcon4 02/02/2026-2
-//  ContentView.swift
-//  
-//  https://github.com/iypc-team/Playgrounds/tree/main/Defcon4.swiftpm
-//  
-//  
+// Defcon4 02/02/2026-3
+// ContentView.swift
+// Created: 2026-02-02
+// Repository: https://github.com/iypc-team/Playgrounds/blob/main/Defcon4.swiftpm/ContentView.swift
 
 import SwiftUI
 import SceneKit
 
 struct ContentView: View {
     @StateObject var viewModel = SceneViewModel()
-    
+    //  Initializer for conditional binding must have Optional type, not 'SCNScene'
     var body: some View {
-        SceneKitView(scene: viewModel.scene, sceneModel: viewModel.sceneModel)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .accessibilityLabel("3D Fighter Scene")
-            .onAppear {
-                
-            }
-            .onDisappear {
-                
-            }
+        if let scene = viewModel.scene {
+            SceneKitView(scene: scene, sceneModel: viewModel.sceneModel)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .accessibilityLabel("3D Fighter Scene")
+        } else {
+            Text("Loading 3D Fighter Scene...")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
 }
 
