8139bbf3c47afa1f95b5ef92f26a34cfca419bbe
Ralph Krueger <ralph.krueger@icloud.com>
2026-02-06 15:17:08 -0600

    Airplane  02/06/2026-8

--- Airplane.swiftpm/ContentView.swift
+++ Airplane.swiftpm/ContentView.swift
@@ -1,4 +1,4 @@
-//  Airplane  02/06/2026-7
+//  Airplane  02/06/2026-8
 //  AirplaneView.swift
 //  Repo:  https://github.com/iypc-team/Playgrounds/tree/main/Airplane.swiftpm
 
@@ -37,7 +37,7 @@ struct ContentView: View {
                     if isContinuousRotating {
                         vm.stopContinuousRotation()
                     } else {
-                        vm.startContinuousRotation(axis: SIMD3<Float>(0, 1, 0), speed: 1.0)  // Always rotate about Y-axis
+                        vm.startContinuousRotation(axis: SIMD3<Float>(0, 1, 0), speed: 0.25)  // Reduced speed for slower rotation
                     }
                     isContinuousRotating.toggle()
                 }
