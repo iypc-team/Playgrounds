95353a509a4033d8882b59187ef3667d219dafea
Ralph Krueger <ralph.krueger@icloud.com>
2026-02-08 15:50:05 -0600

    Defcon4 02/08/2026-3

--- Defcon4.swiftpm/Package.swift
+++ Defcon4.swiftpm/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             displayVersion: "1.0",
             bundleVersion: "1",
             appIcon: .placeholder(icon: .plane),
-            accentColor: .presetColor(.orange),
+            accentColor: .presetColor(.green),
             supportedDeviceFamilies: [
                 .pad,
                 .phone
