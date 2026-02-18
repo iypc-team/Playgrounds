a5bc1f74cbc246fea6124a0f708531debe19dd34
Ralph Krueger <ralph.krueger@icloud.com>
2026-02-18 11:59:37 -0600

    ParticleSystem 02/18/2026-3

--- ParticleSysyem.swiftpm/ContentView.swift
+++ ParticleSysyem.swiftpm/ContentView.swift
@@ -1,38 +1,34 @@
-//  ParticleSystem 02/18/2026-2
+//  ParticleSystem 02/18/2026-3
 //  ContentView.swift
 //  Repo:  https://github.com/iypc-team/Playgrounds/tree/main/ParticleSysyem.swiftpm
-//  
-//  WTF:  https://www.youtube.com/watch?v=raR-hDgzoFg
-//  
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var particleSystem = ParticleSystem()
+    
     var body: some View {
-        TimelineView<AnimationTimelineSchedule, { timeline in }
+        TimelineView(.animation) { timeline in
             Canvas { context, size in
                 let timelineDate = timeline.date.timeIntervalSinceReferenceDate
                 particleSystem.update(date: timelineDate)
                 
+                let image = particleSystem.image
                 for particle in particleSystem {
                     let xPos = particle.x * size.width
                     let yPos = particle.y * size.height
-                    
-                    context.draw(particleSystem.image, at: CGPoint(x: xPos, y: yPos))
+                    context.draw(image, at: CGPoint(x: xPos, y: yPos))
                 }
             }
         }
         .ignoresSafeArea()
-        .background(.black)
+        .background(Color.black)
     }
 }
 
-
-struct ContentView_Preview: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
     }
 }
-
--- ParticleSysyem.swiftpm/ParticleSystem.swift
+++ ParticleSysyem.swiftpm/ParticleSystem.swift
@@ -1,26 +1,43 @@
 //  ParticleSystem.swift
-//  
+//  Updated: make ParticleSystem conform to Sequence by returning an Iterator snapshot
 
 import SwiftUI
-import Swift
+import Foundation
 
-struct Particle: Hashable {
+struct Particle {
     let x: Double
     let y: Double
-    let creationDate = Date.now.timeIntervalSinceReferenceDate
+    let creationDate: TimeInterval = Date.now.timeIntervalSinceReferenceDate
 }
 
-class ParticleSystem: Sequence, IteratorProtocol {
-    let image = Image("star.png")
-    var particles = Set<Particle>()
+final class ParticleSystem: Sequence {
+//    let image = Image("star")
+    let image = Image("spark")
+    private(set) var particles: [Particle] = []
     var center = UnitPoint.center
     
     func update(date: TimeInterval) {
         let newParticle = Particle(x: center.x, y: center.y)
-        particles.insert(newParticle)
+        particles.append(newParticle)
     }
-    func makeIterator() -> Self.Iterator {
-         
+    
+    struct Iterator: IteratorProtocol {
+        private var index = 0
+        private let snapshot: [Particle]
+        
+        init(_ particles: [Particle]) {
+            self.snapshot = particles
+        }
+        
+        mutating func next() -> Particle? {
+            guard index < snapshot.count else { return nil }
+            defer { index += 1 }
+            return snapshot[index]
+        }
+    }
+    
+    func makeIterator() -> Iterator {
+        // return a snapshot so iteration is stable while the system may mutate
+        return Iterator(particles)
     }
 }
-
