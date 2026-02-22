34828b0180e88f18bffc361ded95d60abf2704b1
Ralph Krueger <ralph.krueger@icloud.com>
2026-02-22 09:41:52 -0600

    iCloudDrive 02/22/2026-initial commit

---
+++ iCloudDrive.swiftpm/ContentView.swift
@@ -0,0 +1,16 @@
+//  iCloudDrive 02/22/2026-initial commit
+//  ContentView.swift
+//  Repo:  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
