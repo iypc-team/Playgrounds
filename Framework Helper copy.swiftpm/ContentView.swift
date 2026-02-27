533cd123b57bc756b04188dd6af007d292f38ea8
Ralph Krueger <ralph.krueger@icloud.com>
2026-02-27 16:23:24 -0600

    Framework Helper copy  02/27/2026-2

--- Framework Helper copy.swiftpm/ContentView.swift
+++ Framework Helper copy.swiftpm/ContentView.swift
@@ -1,4 +1,4 @@
-//  Framework Helper copy  02/27/2026-1
+//  Framework Helper copy  02/27/2026-2
 //  
 //   https://github.com/iypc-team/Playgrounds/tree/main/Framework%20Helper%20copy.swiftpm
 //
--- Framework Helper copy.swiftpm/MethodViewModel.swift
+++ Framework Helper copy.swiftpm/MethodViewModel.swift
@@ -1,9 +1,13 @@
-// MethodViewModel.swift
+//   MethodViewModel.swift
 //  
+
+// MethodViewModel.swift
+
+// MethodViewModel.swift
 
 import SwiftUI
 
-class MethodViewModel: ObservableObject {
+final class MethodViewModel: ObservableObject {
     @Published var methods: [String] = []
     let framework: Framework
     
@@ -14,18 +18,46 @@ class MethodViewModel: ObservableObject {
     
     @MainActor
     func fetchMethods() async {
-//        print("func fetchMethods()")
         let currentLibrary = framework.name
-//        print("currentLibrary: \(currentLibrary)")
         
-        if let url = Bundle.main.url(forResource: "methods", withExtension: "json"),
-           let data = try? Data(contentsOf: url),
-           let json = try? JSONSerialization.jsonObject(with: data) as? [String: [String]] {
-            methods = json[currentLibrary] ?? ["No methods available for this framework"]
-        } else {
-            methods = ["init()", "deinit()", "someMethod(param:)"]
+        guard
+            let url = Bundle.main.url(forResource: "methods", withExtension: "json"),
+            let data = try? Data(contentsOf: url),
+            let json = try? JSONSerialization.jsonObject(with: data) as? [String: [String]]
+        else {
+            methods = ["No methods available for this framework"]
+            return
         }
-        print("Fetching methods for \(currentLibrary)")
-        print("methodsArray: \(methods)")
+        
+        methods = json[currentLibrary, default: ["No methods available for this framework"]]
     }
 }
+
+//import SwiftUI
+//
+//class MethodViewModel: ObservableObject {
+//    @Published var methods: [String] = []
+//    let framework: Framework
+//    
+//    init(framework: Framework) {
+//        self.framework = framework
+//        Task { await fetchMethods() }
+//    }
+//    //  in MethodViewModel the function 'fetchMethods()' is supposed to create a list of all methods within 'framework'
+//    @MainActor
+//    func fetchMethods() async {
+//        print("func fetchMethods()")
+//        let currentLibrary = framework.name
+//        print("currentLibrary: \(currentLibrary)")
+//        
+//        if let url = Bundle.main.url(forResource: "methods", withExtension: "json"),
+//           let data = try? Data(contentsOf: url),
+//           let json = try? JSONSerialization.jsonObject(with: data) as? [String: [String]] {
+//            methods = json[currentLibrary] ?? ["No methods available for this framework"]
+//        } else {
+//            methods = ["init()", "deinit()", "someMethod(param:)"]
+//        }
+//        print("Fetching methods for \(currentLibrary)")
+//        print("methodsArray: \(methods)")
+//    }
+//}
