e91cbd64d4dc4f82d98fdac33f4a241871964609
Ralph Krueger <ralph.krueger@icloud.com>
2026-03-01 14:55:43 -0600

    FileManager copy 03/01/2026-initial commit

---
+++ FileManager copy.swiftpm/MyApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import Foundation
+import Combine
+import PlaygroundSupport
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
