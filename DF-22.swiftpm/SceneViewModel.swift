e80e615e410ff7231a8bfb6f2da9a6155121cfc4
Ralph Krueger <ralph.krueger@icloud.com>
2026-03-12 11:41:45 -0500

    DF-22  03/12/2026-1

--- DF-22.swiftpm/SceneViewModel.swift
+++ DF-22.swiftpm/SceneViewModel.swift
@@ -29,6 +29,27 @@ class SceneViewModel: ObservableObject {
         motionManager.stopUpdates()
     }
     
+    func ghostEffect(scene: SCNScene) -> SCNNode {
+        // https://stackoverflow.com/questions/43843110/ios-scenekit-add-fresnel-effect-to-material-transparency
+        let sphere = SCNSphere(radius: 8)
+        sphere.segmentCount = 64
+        
+        let material = SCNMaterial()
+        // material.diffuse.contents = UIColor.clear
+        material.diffuse.contents = UIColor.black
+        material.reflective.contents = UIColor(red: 0, green: 0.764, blue: 1, alpha: 1)
+        material.reflective.intensity = 3
+        material.transparent.contents = UIColor.black.withAlphaComponent(0.3)
+        material.transparencyMode = .default
+        material.fresnelExponent = 4
+        sphere.materials = [material]
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(0, 0, 0)
+        
+        return sphereNode
+    }
+    
     private func setupScene() {
         // Add camera
         let cameraNode = SCNNode()
