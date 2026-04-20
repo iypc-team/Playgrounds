217d43e41db71e16eda6c3ae9c4c70289a7d0f38
Ralph Krueger <ralph.krueger@icloud.com>
2026-04-20 09:12:16 -0500

    CoreMotion_Attitude copy initial commit

---
+++ CoreMotion_Attitude copy.swiftpm/MyApp.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//CoreMotionApp/
+//├── MotionViewModel.swift
+//├── WaveView.swift
+//└── CoreMotionApp.swift
